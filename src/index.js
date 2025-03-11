da87b54e5e4376994f87d09d7161bc2e30337b94
Asad Shah <asad.shah@3dlogistix.com>
2025-03-11 10:05:43 +0500

    UpdatedPipeline&RBACCode

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const startServer = async () => {
         method: 'get',
         maxBodyLength: Infinity,
         //url: 'http://localhost:3001/role/services',
-        url: 'http://prod-rbac-1011040531.ap-southeast-2.elb.amazonaws.com',
+        url: 'http://rbaclb-1460536057.ap-southeast-2.elb.amazonaws.com/role/services',
         headers: { }
       };
       axios.request(config)
