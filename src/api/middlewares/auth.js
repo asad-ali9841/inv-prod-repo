4ce70ed73be741a73abb6cdd026ed4df20425be4
usamaayub1 <usama.ayub@3dlogistix.com>
2025-03-14 19:19:20 +0500

    Changed unauth status code to 401

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -6,10 +6,10 @@ module.exports = async (req,res,next) => {
     if(isAuthorized){
         return next();
     }
-    return res.status(403).json({
+    return res.status(401).json({
         status: 0,  // 0 for success, 0 for error
         type: 'error',  // success/error/ info / error
-        responseMessage: "You are not autheticated. Please try logging in again", // some description of the response
+        responseMessage: "You are not authenticated. Please try logging in again", // some description of the response
         data:{}, // data if it is there
         errorMessage:"", // error message, what is the reason of the error
     })
